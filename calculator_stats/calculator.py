229eb1c30ca3ffeb3dcd6919a10bfa37440ec012
Felipe Di Placido <diplacidofelipe@gmail.com>
2024-04-11 10:01:50 -0300

    Implementacion y uso de la funcion top_scorer.

--- calculator_stats/calculator.py
+++ calculator_stats/calculator.py
@@ -1,6 +1,6 @@
 def generate_statistics(names, goals, goals_avoided, assists):
     """
-    This function processes the statistics and enters them into a list of dictionaries
+    This function processes the statistics and enters them into a list of dictionaries.
 
     Args:
         names: String with the name of each player.
@@ -9,12 +9,25 @@ def generate_statistics(names, goals, goals_avoided, assists):
         assists: List with the assists of each player.
         
     Return
-        A list of dictionaries with the statistics of each player
+        A list of dictionaries with the statistics of each player.
     """
     
     names_list = names.split(", ")
-    player_stats = list(map(lambda name, goal, goal_avoided, assist: 
+    players_stats = list(map(lambda name, goal, goal_avoided, assist: 
         {"name": name, "goals": goal, "goals_avoided": goal_avoided, "asissts": assist},
         names_list, goals, goals_avoided, assists))
     
-    return player_stats
+    return players_stats
+
+def top_scorer (players_stats):
+    """
+    This function finds the player with the most goals and returns his name and number of goals.
+
+    Args:
+        players_stats: List of dictionaries with the statistics of each player.
+    
+    Return:
+        Tuple with the name and goals of the scorer.
+    """
+    scorer = max (players_stats, key= lambda x: x["goals"])
+    return scorer["name"], scorer["goals"]
